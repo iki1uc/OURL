be1c8086995bd34bd38d14f3f90ec6203f94dee3
iki1uc <entertrainer@web.de>
2026-08-23 15:11:55 +0200

    Update brand.js

--- brand.js
+++ brand.js
@@ -1,23 +1,18 @@
 // brand.js
 // EVOLUTION-Axiom für OURL / iki1uc
-// Keine Demokratie-Achse, kein Glaubens-Axiom – nur Energie
 
-// Mana = innere Energie
 function MANA(qi, iqq) {
-    return (qi + iqq) * 0.5; // innere Reinheit
+    return (qi + iqq) * 0.5;
 }
 
-// Aura = äußere Energie
 function AURA(qi, iqq) {
-    return (qi * 0.3) + (iqq * 0.7); // äußere Ausstrahlung
+    return (qi * 0.3) + (iqq * 0.7);
 }
 
-// Vector = Bewegungsenergie
 function VECTOR_ENERGIE(qi, iqq) {
-    return (qi * 0.6) + (iqq * 0.4); // reine Bewegung
+    return (qi * 0.6) + (iqq * 0.4);
 }
 
-// Evolution = Mana + Aura + Vector
 function EVOLUTION(qi, iqq) {
     const mana = MANA(qi, iqq);
     const aura = AURA(qi, iqq);
